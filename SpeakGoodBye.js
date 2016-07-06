270f24c700c8fd4e13aece9b4a9b47ab6565cf28
CedricHadjian <cedrichage95@gmail.com>
2016-07-06 11:46:20 +0300

    initializing

---
+++ SpeakGoodBye.js
@@ -0,0 +1,13 @@
+
+(function (window) {
+
+var byeSpeaker = {
+	speak: function (name){
+		console.log(speakWord + " " + name);
+	}
+}
+
+var speakWord = "Good Bye";
+
+	window.byeSpeaker = byeSpeaker;
+})(window);
---
+++ SpeakHello.js
@@ -0,0 +1,12 @@
+(function (window) {
+
+ var helloSpeaker = {
+ 	speak: function(name){
+ 		console.log(speakWord+" "+name);
+ 	}
+ }
+
+var speakWord = "Hello";
+
+window.helloSpeaker = helloSpeaker;
+})(window);
